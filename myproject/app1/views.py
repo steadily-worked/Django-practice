df76f739a2a7a30e30887136593f4c439d156947
박상민 <iamsangminpark@gmail.com>
2021-05-18 09:34:59 +0900

    Django practice - wordcount

---
+++ myproject/app1/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def count(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    return render(request, 'count.html', {'alltext': entered_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
