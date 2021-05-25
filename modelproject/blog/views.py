e5165103abcfaff03f59ee6d765ecd85f7899a1d
박상민 <iamsangminpark@gmail.com>
2021-05-25 14:42:21 +0900

    Django CRUD ~R

---
+++ modelproject/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+def home(request):
+    blogs = Blog.objects.all()
+    return render(request, 'home.html', {'blogs': blogs})
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'detail.html', {'blog': blog})
